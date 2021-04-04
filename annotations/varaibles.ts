c6fb2e933e9bb10e4de8b05f6e618b566b0f492e
saymarizvi <rizvisayma@yahoo.com>
2021-04-04 23:39:50 +0530

    where to add type annotations

--- annotations/varaibles.ts
+++ annotations/varaibles.ts
@@ -41,3 +41,28 @@ const coordinates: { x: number; y: number } = JSON.parse(json);
 
 
 console.log(coordinates);
+
+
+//2) When we declare varaible at one line and initaialize it late
+
+let words = ['red', 'green', 'blue'];
+
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+//3) When a varaible caanot be infered correctly
+
+let numbers = [-10 , -1 , 12];
+
+let numberAboveZero : boolean | number = false ;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] >0 ) {
+        numberAboveZero = numbers[i];
+    }
+}
