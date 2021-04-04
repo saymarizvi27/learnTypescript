2e5c55fb9ac52904f215aee2d3d1ea4e8aba2ee1
saymarizvi <rizvisayma@yahoo.com>
2021-04-04 11:16:58 +0530

    varables

---
+++ annotations/varaibles.ts
@@ -0,0 +1,27 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in Object
+let now: Date = new Date();
+
+//Array 
+let colour: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+//Classes
+
+class Car {}
+
+let car: Car = new Car();
+
+//Object literal 
+
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+}
---
+++ types.ts
@@ -0,0 +1,10 @@
+const today = new Date();
+today.getDay();
+
+
+const person = {
+    age: 20
+};
+
+class Color { }
+const red = new Color();
